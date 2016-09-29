87fe7632f79cff5e952d53db03d4d5146fe67566
momoiroshikibu <momoiroshikibu@gmail.com>
2016-09-30 08:58:14 +0900

    data-type.lisp

---
+++ data-type.lisp
@@ -0,0 +1,30 @@
+(eq 'fooo 'FoOo)
+; T
+
+(+ 1 1.0)
+
+
+(expt 53 53)
+
+(/ 4 6)
+
+(/ 4.0 6)
+
+(princ "Tutti Frutti")
+
+(princ "He yelled \"Stop that thief!\" from the busy street.")
+
+
+'(expt 2 3)
+
+(cons 'chicken 'cat)
+
+(cons 'chicken 'nil)
+
+(cons 'chicken ())
+
+(cons 'pork '(beek chicken))
+(cons 'beef (cons 'chicken ()))
+
+
+(cons 'pork (cons 'beef (cons 'chicken ())))
