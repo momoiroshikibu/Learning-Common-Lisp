e713611d8fead33d5b588fd1d8c0e832f6f1928e
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-02 23:09:26 +0900

    update file

--- file.lisp
+++ file.lisp
@@ -3,3 +3,12 @@
                  :direction :output
                  :if-exists :supersede)
   (princ "Hello File!" my-stream))
+
+
+(defun dot->png (fname thunk)
+  (with-open-file (*standard-output*
+                   (concatenate 'string fname ".dot")
+                   :direction :output
+                   :if-exists :supersede)
+    (funcall thunk))
+  (ext:shell (concatenate 'string "dot -Tpng -O" fname)))
