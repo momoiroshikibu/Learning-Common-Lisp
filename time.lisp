8b5d91df950f259b957e8d4f2b06857f182d6df6
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-04 08:12:53 +0900

    time

---
+++ time.lisp
@@ -0,0 +1,14 @@
+;; time
+;; コードの実行時間を測る
+
+(time (print 1))
+;; 1
+;; Evaluation took:
+;;   0.000 seconds of real time
+;;   0.000019 seconds of total run time (0.000017 user, 0.000002 system)
+;;   100.00% CPU
+;;   46,170 processor cycles
+;;   0 bytes consed
+;;
+;; 1
+
