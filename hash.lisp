c05915b7a99530144454771fc784711e52cf049e
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-03 20:56:29 +0900

    hash

---
+++ hash.lisp
@@ -0,0 +1,33 @@
+;; ハッシュテーブル
+;; alistに似ている。
+;; キーに結び付けられた値を取り出す。alistよりも速い。
+
+(make-hash-table)
+
+
+;; alistと同じようにハッシュテーブルは要素をキーと値の組み合わせで格納する
+;; キーを使って要素を取り出すのはgethash
+(defparameter x (make-hash-table))
+(gethash 'yup x)
+;; NIL
+;; NIL
+
+;; gethashは複数の値を返す。
+;; 最初に返される値: ハッシュテーブルに保存されていた値
+;; 2番目の値: キーがテーブルの中に存在したかどうかを示す
+
+
+(defparameter x (make-hash-table))
+(setf (gethash 'yup x) '25)
+(gethash 'yup x)
+;; 25, T
+
+
+(defparameter *drink-order* (make-hash-table))
+(setf (gethash 'bill *drink-order*) 'double-espresso)
+(setf (gethash 'lisa *drink-order*) 'small-drip-coffee)
+(setf (gethash 'john *drink-order*) 'medium-latte)
+
+(gethash 'lisa *drink-order*)
+;; SMALL-DRIP-COFFEE
+;; T
