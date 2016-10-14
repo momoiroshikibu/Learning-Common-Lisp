7f214f7d4d61f1a3133749d70c97da15a3e443cc
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-14 12:43:08 +0900

    max

---
+++ max.lisp
@@ -0,0 +1,15 @@
+;; max
+
+(max 3) ; 3
+(max 1 2) ; 2
+
+(max 0 0) ; 0
+(max 1.0 0.9) ; 1.0
+
+(max 1 1.0) ; 1
+(max 1.0 1) ; 1.0
+
+(max -1 0) ; 0
+
+(max '(1 2 3)) ; Error The value (1 2 3) is not of type REAL
+
