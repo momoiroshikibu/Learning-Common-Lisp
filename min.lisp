935f37ac8cca11ec4e73ed550ba37c6ccd42c362
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-14 12:43:57 +0900

    min

---
+++ min.lisp
@@ -0,0 +1,15 @@
+;; min
+
+(min 3) ; 3
+(min 1 2) ; 1
+
+(min 0 0) ; 0
+(min 1.0 0.9) ; 0.9
+
+(min 1 1.0) ; 1
+(min 1.0 1) ; 1.0
+
+(min -1 0) ; -1
+
+(min '(1 2 3)) ; Error The value (1 2 3) is not of type REAL
+
