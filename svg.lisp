4697f2e8e142671c18ceb4e030e152b77a0ea6d2
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-15 23:58:34 +0900

    output svg

--- svg.lisp
+++ svg.lisp
@@ -101,3 +101,16 @@
 
 (random-walk 100 10)
 
+
+(with-open-file (*standard-output* "random_walk.svg"
+                                   :direction :output
+                                   :if-exists :supersede)
+  (svg 400 200 (loop repeat 10
+                  do (polygon (append '((0 . 200))
+                                      (loop for x
+                                         for y in (random-walk 100 400)
+                                         collect (cons x y))
+                                      '((400 . 200)))
+                              (loop repeat 3
+                                 collect (random 256))))))
+
