7a039699d9d3e6aae8865bcb98dd4d66ebf5b660
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-21 19:54:53 +0900

    memoize

---
+++ memoize.lisp
@@ -0,0 +1,9 @@
+;; メモ化は関数型スタイルのコードにしか使えない
+;; 関数が受け取った引数と、その結果を毎回記録しておく
+;; 関数が以前受け取ったのと同じ引数でまた呼ばれたら、結果をもう一度計算せずに、計算済みの値を返す。
+
+(let ((old-neighbors (symbol-function 'neighbors))
+      (previous (make-hash-table)))
+  (defun neighbors (pos)
+    (or (gethash pos previous)
+        (setf (gethash pos previous) (funcall old-neighbors pos)))))
