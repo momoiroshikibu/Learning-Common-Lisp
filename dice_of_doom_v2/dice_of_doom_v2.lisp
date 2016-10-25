03f3b8b36a1f0c1bcf745e39dedb3e6044db279b
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-25 21:14:02 +0900

    ai-level

--- dice_of_doom_v2/dice_of_doom_v2.lisp
+++ dice_of_doom_v2/dice_of_doom_v2.lisp
@@ -79,3 +79,18 @@
                                  (limit-tree-depth (cadr move) (1- depth))))
                          (caddr tree)))))
 
+
+(defparameter *ai-level* 4)
+
+(defun handle-computer (tree)
+  (let ((ratings (get-ratings (limit-tree-depth tree *ai-level*)
+                              (car tree))))
+    (cadr (lazy-nth (position (apply #'max ratings) ratings)
+                    (caddr tree)))))
+
+
+(defun play-vs-computer (tree)
+  (print-info tree)
+  (cond ((lazy-null (caddr tree)) (announce-winner (cadr tree)))
+        ((zerop (car tree)) (play-vs-computer (handle-human tree)))
+        (t (play-vs-computer (handle-computer tree)))))
