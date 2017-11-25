677fd876b561b4df1bc89a29ea0267be664591ee
momoiroshikibu <momoiroshikibu@gmail.com>
2017-11-25 22:09:39 +0900

    remove-if-not lambda

--- remove-if-not.lisp
+++ remove-if-not.lisp
@@ -10,3 +10,5 @@
 ;; from Practical Common Lisp
 ;; http://www.gigamonkeys.com/book/practical-a-simple-database.html
 (remove-if-not #'evenp '(1 2 3 4 5 6 7 8 9 10))
+
+(remove-if-not #'(lambda (x) (= 0 (mod x 2))) '(1 2 3 4 5 6 7 8 9 10))
