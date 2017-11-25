0a68730234376a03f6d9ac030b597bd6b439565b
momoiroshikibu <momoiroshikibu@gmail.com>
2017-11-25 22:02:22 +0900

    remove-if-not

--- remove-if-not.lisp
+++ remove-if-not.lisp
@@ -5,3 +5,8 @@
 
 (remove-if-not #'2= '(1 2 3))           ; (2)
 (remove-if-not #'2= '(2 2 2))           ; (2 2 2)
+
+
+;; from Practical Common Lisp
+;; http://www.gigamonkeys.com/book/practical-a-simple-database.html
+(remove-if-not #'evenp '(1 2 3 4 5 6 7 8 9 10))
